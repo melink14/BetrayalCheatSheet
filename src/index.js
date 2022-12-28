42943d3f2376542e96d2c1f4ad9d732cc7179d74
Erek Speed <melink14@gmail.com>
2022-12-28 10:32:47 +0000

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import BetrayalCheatSheet from "./components/BetrayalCheatSheet";
-
-ReactDOM.render(<BetrayalCheatSheet />, document.getElementById('root'));
